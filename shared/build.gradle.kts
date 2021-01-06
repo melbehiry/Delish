79299653cb3f9fccc0c4cb97726aeced4987efae
elbehiry <m.elbehiry44@gmail.com>
2021-01-06 15:52:32 +0200

    Added shared module dependencies

---
+++ shared/build.gradle.kts
@@ -0,0 +1,138 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdkVersion(Versions.COMPILE_SDK)
+    defaultConfig {
+        minSdkVersion(Versions.MIN_SDK)
+        targetSdkVersion(Versions.TARGET_SDK)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
+        consumerProguardFiles("consumer-proguard-rules.pro")
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.incremental"] = "true"
+            }
+        }
+    }
+
+
+    lintOptions {
+        disable("InvalidPackage", "MissingTranslation")
+        // Version changes are beyond our control, so don't warn. The IDE will still mark these.
+        disable("GradleDependency")
+        // Timber needs to update to new Lint API
+        disable("ObsoleteLintCustomCheck")
+    }
+
+    // debug and release variants share the same source dir
+    sourceSets {
+        getByName("debug") {
+            java.srcDir("src/debugRelease/java")
+        }
+        getByName("release") {
+            java.srcDir("src/debugRelease/java")
+        }
+    }
+
+    // Some libs (such as androidx.core:core-ktx 1.2.0 and newer) require Java 8
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // To avoid the compile error: "Cannot inline bytecode built with JVM target 1.8
+    // into bytecode that is being built with JVM target 1.6"
+    kotlinOptions {
+        val options = this
+        options.jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        val urlName = "UNSPLASH_BASE_URL"
+        val baseUrl = "\"https://api.unsplash.com/\""
+        val keyName = "UNSPLASH_KEY"
+        val keyValue = "\"42b524e3c38fcf0c887e3dd2c383ae1c7f64011566b586d48bc93549632eff46\""
+        getByName("release") {
+            buildConfigField("String", urlName, baseUrl)
+            buildConfigField(
+                "String",
+                keyName,
+                keyValue
+            )
+        }
+        getByName("debug") {
+            buildConfigField("String", urlName, baseUrl)
+            buildConfigField(
+                "String",
+                keyName,
+                keyValue
+            )
+        }
+    }
+}
+
+dependencies {
+    api(platform(project(":depconstraints")))
+    kapt(platform(project(":depconstraints")))
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    // AppCompat
+    implementation(Libs.APPCOMPAT)
+
+    // Architecture Components
+    implementation(Libs.LIFECYCLE_LIVE_DATA_KTX)
+    implementation(Libs.LIFECYCLE_VIEW_MODEL_KTX)
+    implementation(Libs.ROOM_KTX)
+    implementation(Libs.ROOM_RUNTIME)
+    kapt(Libs.ROOM_COMPILER)
+    testImplementation(Libs.ARCH_TESTING)
+
+
+    // Utils
+    api(Libs.TIMBER)
+    implementation(Libs.GSON)
+    implementation(Libs.CORE_KTX)
+
+    // OkHttp
+    implementation(Libs.OKHTTP)
+    implementation(Libs.OKHTTP_LOGGING_INTERCEPTOR)
+
+    // Retrofit
+    api(Libs.RETROFIT)
+    api(Libs.MOSHI)
+    api(Libs.MOSHI_KOTLIN)
+    api(Libs.MOSHI_RETROFIT)
+    api(Libs.KOTCHI)
+    kapt(Libs.KOTCHI_COMPILER)
+
+    // Kotlin
+    implementation(Libs.KOTLIN_STDLIB)
+
+    // Coroutines
+    api(Libs.COROUTINES)
+    testImplementation(Libs.COROUTINES_TEST)
+
+    // Dagger Hilt
+    implementation(Libs.HILT_ANDROID)
+    kapt(Libs.HILT_COMPILER)
+
+
+    // Unit tests
+    testImplementation(Libs.JUNIT)
+    testImplementation(Libs.HAMCREST)
+    testImplementation(Libs.MOCKITO_CORE)
+    testImplementation(Libs.MOCKITO_KOTLIN)
+
+    // unit tests livedata
+    testImplementation(Libs.ARCH_TESTING)
+}
