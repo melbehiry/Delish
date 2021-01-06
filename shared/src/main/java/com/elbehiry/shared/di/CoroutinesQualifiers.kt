eca7ce782438d72ac1f23a083a7ec239c71717f3
elbehiry <m.elbehiry44@gmail.com>
2021-01-06 21:35:25 +0200

    Added Coroutine qualifiers annotations

---
+++ shared/src/main/java/com/elbehiry/shared/di/CoroutinesQualifiers.kt
@@ -0,0 +1,24 @@
+package com.elbehiry.shared.di
+
+import javax.inject.Qualifier
+
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainImmediateDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ApplicationScope
