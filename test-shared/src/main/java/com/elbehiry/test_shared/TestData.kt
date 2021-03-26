0021198b164f947631dbb110d2459eaa8908a313
elbehiry <m.elbehiry44@gmail.com>
2021-03-26 21:05:33 +0200

    Test data with faker lib.

--- test-shared/src/main/java/com/elbehiry/test_shared/TestData.kt
+++ test-shared/src/main/java/com/elbehiry/test_shared/TestData.kt
@@ -16,9 +16,51 @@
 
 package com.elbehiry.test_shared
 
-object RecipesTestData {
+import com.elbehiry.model.CuisineItem
+import com.elbehiry.model.RecipesItem
+import com.github.javafaker.Faker
 
-    val id = 100
-    val recipe = "{id:$id,vegetarian:true}"
-    val recipes = "{recipes:[$recipe]}"
-}
+val RECIPE_ID = Faker().number().digits(3).toInt()
+val recipe = "{id:$RECIPE_ID,vegetarian:true}"
+val recipes = "{recipes:[$recipe]}"
+
+val faker = Faker()
+
+val RECIPE_ITEM = RecipesItem(
+    id = faker.number().digits(3).toInt(),
+    title = faker.lorem().sentence(),
+    sustainable = false,
+    glutenFree = false,
+    veryPopular = false,
+    vegetarian = false,
+    dairyFree = false,
+    veryHealthy = false,
+    vegan = false,
+    cheap = false,
+    spoonacularScore = faker.number().digit().toDouble(),
+    aggregateLikes = faker.number().digit().toInt(),
+    sourceName = faker.lorem().word(),
+    creditsText = faker.lorem().sentence(),
+    readyInMinutes = faker.number().digit().toInt(),
+    image = faker.internet().image(),
+    percentCarbs = faker.number().digit().toDouble(),
+    percentProtein = faker.number().digit().toDouble(),
+    percentFat = faker.number().digit().toDouble(),
+    nutrientsAmount = faker.number().digit().toDouble(),
+    nutrientsName = faker.lorem().word()
+)
+
+val RECIPES_ITEMS = listOf(
+    RECIPE_ITEM.copy(id = faker.number().digits(3).toInt()),
+    RECIPE_ITEM.copy(id = faker.number().digits(3).toInt()),
+    RECIPE_ITEM.copy(id = faker.number().digits(3).toInt()),
+    RECIPE_ITEM.copy(id = faker.number().digits(3).toInt())
+)
+
+val CUISINE_ITEM = CuisineItem(
+    image = faker.internet().image(),
+    title = faker.lorem().sentence(),
+    color = faker.color().hex()
+)
+
+val CUISINES_ITEMS = listOf(CUISINE_ITEM, CUISINE_ITEM, CUISINE_ITEM)
