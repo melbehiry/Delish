7315720add6edec4e42b9d70b0203c1fdb92d36c
elbehiry <m.elbehiry44@gmail.com>
2021-04-05 15:55:07 +0200

    Refactor the onboarding ui test

--- mobile/src/androidTest/java/com/elbehiry/delish/onboarding/OnBoardingTest.kt
+++ mobile/src/androidTest/java/com/elbehiry/delish/onboarding/OnBoardingTest.kt
@@ -1,12 +1,9 @@
 package com.elbehiry.delish.onboarding
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.elbehiry.delish.R
@@ -43,13 +40,13 @@ class OnBoardingTest {
     @Test
     fun skipButtonIsDisplayedAndWithTextSkipTest() {
         findSkipButton().assertIsDisplayed()
-        findLetsGoButton().assertIsNotDisplayed()
+        findLetsGoButton().assertDoesNotExist()
     }
 
     @Test
     fun nextButtonIsDisplayedAndWithTextNextTest() {
         findNextButton().assertIsDisplayed()
-        findLetsGoButton().assertIsNotDisplayed()
+        findLetsGoButton().assertDoesNotExist()
     }
 
     @Test
@@ -57,43 +54,25 @@ class OnBoardingTest {
         val firstOnBoardingItem = OnBoardingProvider.onBoardingDataList[0]
         composeTestRule.onNodeWithText(
             activity.getString(firstOnBoardingItem.titleId)
-        )
+        ).assertIsDisplayed()
         composeTestRule.onNodeWithText(
             activity.getString(firstOnBoardingItem.DescriptionId)
-        )
+        ).assertIsDisplayed()
+        findLetsGoButton().assertDoesNotExist()
     }
 
-    @Test
-    fun secondPageDescriptionTitleAndDescIsDisplayedTest() {
-        findNextButton().performClick()
-        val firstOnBoardingItem = OnBoardingProvider.onBoardingDataList[1]
-        composeTestRule.onNodeWithText(
-            activity.getString(firstOnBoardingItem.titleId)
-        )
-        composeTestRule.onNodeWithText(
-            activity.getString(firstOnBoardingItem.DescriptionId)
-        )
-    }
+    private fun findSkipButton() = composeTestRule.onNodeWithText(
+        activity.getString(R.string.skip), ignoreCase = true
+    )
 
-    @Test
-    fun thirdPageDescriptionTitleAndDescIsDisplayedTest() {
-        findNextButton().performClick()
-        findNextButton().performClick()
-        val firstOnBoardingItem = OnBoardingProvider.onBoardingDataList[2]
-        composeTestRule.onNodeWithText(
-            activity.getString(firstOnBoardingItem.titleId)
-        )
-        composeTestRule.onNodeWithText(
-            activity.getString(firstOnBoardingItem.DescriptionId)
-        )
-        findLetsGoButton().assertIsDisplayed()
-        findSkipButton().assertIsNotDisplayed()
-    }
+    private fun findNextButton() = composeTestRule.onNodeWithText(
+        activity.getString(R.string.next), ignoreCase = true
+    )
 
-    private fun findSkipButton() = composeTestRule.onNodeWithText(activity.getString(R.string.skip))
-    private fun findNextButton() = composeTestRule.onNodeWithText(activity.getString(R.string.next))
     private fun findLetsGoButton() =
-        composeTestRule.onNodeWithText(activity.getString(R.string.onBoarding_start))
+        composeTestRule.onNodeWithText(
+            activity.getString(R.string.onBoarding_start), ignoreCase = true
+        )
 
 
 }
