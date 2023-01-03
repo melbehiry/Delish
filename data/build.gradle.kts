1a73960ca585a7ba6fc7c5001cf006ec4b95dbea
el6ehiry@gmail.com <el6ehiry@gmail.com>
2023-01-03 20:51:50 +0100

    Refactor the data build gradle file

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -20,52 +20,8 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
-
-
 android {
-    compileSdk = libs.versions.compile.sdk.version.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.min.sdk.version.get().toInt()
-        targetSdk = libs.versions.target.sdk.version.get().toInt()
-        namespace = "app.delish.data"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-
-        consumerProguardFiles("consumer-proguard-rules.pro")
-
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments["room.incremental"] = "true"
-                arguments["room.schemaLocation"] = "$projectDir/schemas"
-            }
-        }
-    }
-
-    // Some libs (such as androidx.core:core-ktx 1.2.0 and newer) require Java 8
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    // To avoid the compile error: "Cannot inline bytecode built with JVM target 1.8
-    // into bytecode that is being built with JVM target 1.6"
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    sourceSets {
-        getByName("androidTest").assets.srcDirs("$projectDir/schemas")
-    }
-
-    packagingOptions {
-        resources.excludes.add("META-INF/licenses/**")
-        resources.excludes.add("META-INF/AL2.0")
-        resources.excludes.add("META-INF/LGPL2.1")
-    }
+    namespace = "app.delish.data"
 }
 
 ksp {
@@ -76,8 +32,6 @@ ksp {
 dependencies {
     implementation(projects.base)
     api(projects.model)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
 
     api(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
@@ -88,12 +42,7 @@ dependencies {
     api(libs.androidx.room.common)
     api(libs.androidx.paging.common)
     api(libs.timber)
-    implementation(libs.androidx.core.ktx)
 
-//    // Architecture Components
-//    testImplementation(Libs.ARCH_TESTING)
-//
-//
     // OkHttp
     implementation(libs.okhttp)
     implementation(libs.okhttp.interceptor)
@@ -103,44 +52,13 @@ dependencies {
     api(libs.retrofit)
     api(libs.moshi)
     api(libs.retrofit.converter.moshi)
-//    api(Libs.MOSHI_KOTLIN)
 
-//    // Kotlin
-//    implementation(Libs.KOTLIN_STDLIB)
-//
     // Coroutines
     api(libs.coroutines.core)
-//    testImplementation(Libs.COROUTINES_TEST)
-//    implementation(Libs.COROUTINES_PLAY_SERVICE)
 //
     implementation(libs.hilt.library)
     kapt(libs.hilt.compiler)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.hamcrest)
-//    testImplementation(libs.mockito.core)
-//    testImplementation(libs.mockito.kotlin)
-    testImplementation(libs.javafaker)
-//    testImplementation(libs.turbine)
-//    testImplementation(libs.junit.ext)
-    testImplementation(libs.assertj.core)
-    testImplementation(libs.mockk)
-
-    androidTestImplementation(libs.androidx.arc.testing)
-    androidTestImplementation(libs.androidx.test.runner)
-//    androidTestImplementation(libs.junit.ext)
-    androidTestImplementation(libs.assertj.core)
-//    androidTestImplementation(libs.turbine)
-//    androidTestImplementation(libs.room.testing)
-    androidTestImplementation(libs.coroutines.test)
-    androidTestImplementation(libs.javafaker)
-//
-    // unit tests livedata
-    testImplementation(libs.androidx.arc.testing)
-
     // Data store
     api(libs.datastore)
-
-//
-//    implementation(Libs.PLAY_SERVICE_LOCATION)
 }
